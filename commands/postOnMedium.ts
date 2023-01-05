64729c499ba4d79740eba10de1ce36e334ccc513
Rodion Chachura <geekrodion@gmail.com>
2023-01-05 12:33:19 +0400

    refactor

--- commands/postOnMedium.ts
+++ commands/postOnMedium.ts
@@ -28,6 +28,20 @@ interface BlogMetadata {
   github?: string
 }
 
+interface MediumPost {
+  title: string
+  tags?: string[]
+  content: string
+}
+
+type PublishStatus = "public" | "draft"
+
+interface PostOnMediumParams extends MediumPost {
+  contentFormat: "markdown"
+  canonicalUrl: string
+  publishStatus: PublishStatus
+}
+
 const mediumAuthorizationHeader = `Bearer ${process.env.MEDIUM_INTEGRATION_TOKEN}`
 
 interface ParsedMarkdown {
@@ -52,56 +66,8 @@ const streamToBlob = (
   })
 }
 
-const prepareContentForMedium = async (
-  slug: string,
-  content: string,
-  metadata: BlogMetadata
-) => {
-  const insertions: string[] = []
-
-  const images = content.match(/\!\[.*\]\(.*\)/g)
-  await Promise.all(
-    (images || []).map(async (imageToken) => {
-      const imageUrl = imageToken.match(/[\(].*[^\)]/)[0].split("(")[1]
-      if (imageUrl.startsWith("http")) return
-
-      const imagePath = getPostFilePath(slug, imageUrl)
-
-      const mediumImageUrl = await uploadImageToMedium(imagePath)
-      const newImageToken = imageToken.replace(imageUrl, mediumImageUrl)
-      content = content.replace(imageToken, newImageToken)
-    })
-  )
-
-  if (metadata.featuredImage) {
-    const mediumImageUrl = await uploadImageToMedium(
-      getPostFilePath(slug, metadata.featuredImage)
-    )
-    insertions.push(`![](${mediumImageUrl})`)
-  }
-
-  if (metadata.youTubeVideo) {
-    insertions.push(`[👋 **Watch on YouTube**](${metadata.youTubeVideo})`)
-  }
-
-  const resources: string[] = []
-  if (metadata.github) {
-    resources.push(`[🐙 GitHub](${metadata.github})`)
-  }
-  if (metadata.demo) {
-    resources.push(`[🎮 Demo](${metadata.demo})`)
-  }
-  if (resources.length) {
-    insertions.push(resources.join("  |  "))
-  }
-
-  return [...insertions, content].join("\n\n")
-}
-
 interface UploadImageResponse {
-  data: {
-    url: string
-  }
+  url: string
 }
 
 const uploadImageToMedium = async (imagePath: string) => {
@@ -119,48 +85,110 @@ const uploadImageToMedium = async (imagePath: string) => {
     },
   })
 
-  const resp = (await uploadFileResponse.json()) as UploadImageResponse
-  return resp.data.url
+  const { data }: FetchResponse<UploadImageResponse> =
+    await uploadFileResponse.json()
+  return data.url
 }
 
-const postOnMedium = async (slug: string) => {
+const prepareContentForMedium = async (slug: string): Promise<MediumPost> => {
   const markdownFilePath = getPostFilePath(slug, "index.md")
   const markdown = fs.readFileSync(markdownFilePath, "utf8")
   let { body, attributes } = fm(markdown) as ParsedMarkdown
-  body = await prepareContentForMedium(slug, body, attributes)
+  const { featuredImage, youTubeVideo, demo, github, title, keywords } =
+    attributes
 
-  const headers = {
-    "Content-Type": "application/json",
-    Authorization: mediumAuthorizationHeader,
+  const insertions: string[] = []
+
+  const images = body.match(/\!\[.*\]\(.*\)/g)
+  await Promise.all(
+    (images || []).map(async (imageToken) => {
+      const imageUrl = imageToken.match(/[\(].*[^\)]/)[0].split("(")[1]
+      if (imageUrl.startsWith("http")) return
+
+      const imagePath = getPostFilePath(slug, imageUrl)
+
+      const mediumImageUrl = await uploadImageToMedium(imagePath)
+      const newImageToken = imageToken.replace(imageUrl, mediumImageUrl)
+      body = body.replace(imageToken, newImageToken)
+    })
+  )
+
+  if (featuredImage) {
+    const mediumImageUrl = await uploadImageToMedium(
+      getPostFilePath(slug, featuredImage)
+    )
+    insertions.push(`![](${mediumImageUrl})`)
   }
 
+  if (youTubeVideo) {
+    insertions.push(`[👋 **Watch on YouTube**](${youTubeVideo})`)
+  }
+
+  const resources: string[] = []
+  if (github) {
+    resources.push(`[🐙 GitHub](${github})`)
+  }
+  if (demo) {
+    resources.push(`[🎮 Demo](${demo})`)
+  }
+  if (resources.length) {
+    insertions.push(resources.join("  |  "))
+  }
+
+  return {
+    content: [...insertions, body].join("\n\n"),
+    title,
+    tags: keywords,
+  }
+}
+
+const getMediumUser = async () => {
   const userResponse = await fetch("https://api.medium.com/v1/me", {
     method: "GET",
-    headers,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: mediumAuthorizationHeader,
+    },
   })
 
-  const { data: user }: FetchResponse<User> = await userResponse.json()
+  const { data }: FetchResponse<User> = await userResponse.json()
 
+  return data
+}
+
+const postMediumStory = async (userId: string, params: PostOnMediumParams) => {
   const publishStoryRequest = await fetch(
-    `https://api.medium.com/v1/users/${user.id}/posts`,
+    `https://api.medium.com/v1/users/${userId}/posts`,
     {
       method: "POST",
-      headers,
-      body: JSON.stringify({
-        title: attributes.title,
-        contentFormat: "markdown",
-        tags: attributes.keywords,
-        content: body,
-        canonicalUrl: `https://radzion.com/blog/${slug}`,
-        publishStatus: "public",
-      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: mediumAuthorizationHeader,
+      },
+      body: JSON.stringify(params),
     }
   )
 
-  const {
-    data: { url },
-  }: FetchResponse<Story> = await publishStoryRequest.json()
+  const { data }: FetchResponse<Story> = await publishStoryRequest.json()
+
+  return data
+}
+
+const postOnMedium = async (slug: string) => {
+  const mediumPost = await prepareContentForMedium(slug)
+
+  const user = await getMediumUser()
+
+  const { url } = await postMediumStory(user.id, {
+    ...mediumPost,
+    contentFormat: "markdown",
+    canonicalUrl: `https://radzion.com/blog/${slug}`,
+    publishStatus: "public",
+  })
+
   console.log(url)
 }
 
-postOnMedium(process.argv.slice(2)[0])
+const args = process.argv.slice(2)
+
+postOnMedium(args[0])
